46d5f4b6d1cbabcc67140c7115ad3fcd755b2db9
juan <juan1992b.ortiz@gmail.com>
2020-11-16 20:13:34 +0100

    update average

--- average.cs
+++ average.cs
@@ -1,20 +1,26 @@
 using System;
 public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+    static void Main(string[] args)
+    /**we have changed the while structure to make it with a do while and we have formatted the code to make it more readable*/
+    {
+        double a = 0, b, c = 0, d;
+        string s;
+        Console.Write("Insert a mark: ");
+        s = Console.ReadLine(); /** we read the mark as string to check the word "end"	*/
+
+        do
+        {
+            b = Convert.ToDouble(s);
+            c++;   /**counter of numbers*/
+            a += b;  /** sum of all the notes*/
+            Console.Write("Insert a mark: ");
+            s = Console.ReadLine();
+        }
+        while (s != "end");
+        if (c != 0)
+        {
+            d = a / c;  /**calculate the average*/
+            Console.WriteLine("The average is {0}.", d);
+        }
+    }
 }
